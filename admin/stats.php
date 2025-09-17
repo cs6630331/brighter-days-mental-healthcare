91a01b3e5adc5e7c527266e1baa639e8ea1fc649
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-09-17 18:02:47 +0700

    เพิ่มหน้าสถิติ + แก้ไข css เล็กน้อย

---
+++ admin/stats.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include "../components/head.php"; ?>
+	<title>Stats | Admin</title>
+	<link rel="stylesheet" href="../style/admin.css">
+</head>
+<body>
+	<?php
+		$logged_in = true;
+		include "../components/header.php";
+	?>
+	<main>
+		<h1>สถิติ</h1>
+		<form action=".">
+			<label for="doctor">
+				จิตแพทย์ที่ต้องการ
+				<select name="doctor" id="doctor">
+					<option value="-1">-----</option>
+					<?php for ($i = 0; $i < 9; $i++): ?>
+						<option value="1">เพชรกัญญา มีญาณทิพย์</option>
+					<?php endfor; ?>
+				</select>
+			</label>
+		</form>
+		<table class="stats">
+			<thead>
+				<tr>
+					<th>เดือน/ปี</th>
+					<th>จำนวนผู้ลงนัดหมาย</th>
+					<th>จำนวนวันที่นัดหมาย</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php for ($i = 0; $i < 10; $i++): ?>
+					<tr>
+						<td>06/2025</td>
+						<td>95</td>
+						<td>10</td>
+					</tr>
+				<?php endfor; ?>
+			</tbody>
+		</table>
+	</main>
+	<?php include "../components/footer.php"; ?>
+</body>
+</html>
