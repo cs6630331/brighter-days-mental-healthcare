60c07ef654973befacde150e23dc1b3b643de708
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-09-26 22:09:34 +0700

    เพิ่มฟังก์ชันการใช้งานของหน้าการนัดฝั่ง admin

--- admin/appointments.php
+++ admin/appointments.php
@@ -4,6 +4,13 @@
 	<?php include "../components/head.php"; ?>
 	<title>Appointments | Admin</title>
 	<link rel="stylesheet" href="../style/admin.css">
+	<script>
+		function confirmComplete(id, patientName) {
+			if (window.confirm(`คุณต้องการยืนยันว่าการนัดของ ${patientName} เสร็จสิ้นแล้วหรือไม่`)) {
+				location.href = "appointment-done.php?appointment-id=" + id;
+			}
+		}
+	</script>
 </head>
 <body>
 	<?php
@@ -13,21 +20,67 @@
 	<main>
 		<h1>ค้นหานัดหมาย</h1>
 		<form id="search">
-			<label for="name">
+			<label for="q">
 				Name:
-				<input type="text" name="name" id="name">
+				<input
+					type="text"
+					name="q"
+					id="q"
+					<?php if (isset($_GET["q"])): ?>value="<?=$_GET["q"]?>"<?php endif; ?>
+				>
 			</label>
-			<label for="surname">
-				Surname:
-				<input type="text" name="surname" id="surname">
-			</label>
+			<button type="submit">Search</button>
 		</form>
-		<?php for ($i = 0; $i < 10; $i++): ?>
+		<?php
+			include "../connect.php";
+
+			if (isset($_GET["q"]) && !empty($_GET["q"])) {
+				$stmt = $pdo->prepare("
+					SELECT
+					    `_appointment`.`appointment_id`,
+					    CONCAT(`_user`.`user_name`, ' ', `_user`.`user_surname`) AS patient_fullname,
+					    CONCAT(`_doctor`.`doctor_name`, ' ', `_doctor`.`doctor_surname`) AS doctor_fullname,
+					    `_appointment`.`appointment_date`,
+					    `_appointment`.`appointment_time`
+					FROM
+					    `_appointment`
+					JOIN `_doctor` ON `_doctor`.`doctor_id` = `_appointment`.`doctor_id`
+					JOIN `_user` ON `_user`.`user_id` = `_appointment`.`user_id`
+					WHERE `_appointment`.`status` = 'confirmed'
+					GROUP BY
+						`_appointment`.`appointment_id`
+					HAVING
+						patient_fullname LIKE ?;
+				");
+				$q = $_GET["q"] . "%";
+
+				$stmt->bindParam(1, $q);
+			}
+			else {
+				$stmt = $pdo->prepare("
+					SELECT
+					    `_appointment`.`appointment_id`,
+					    CONCAT(`_user`.`user_name`, ' ', `_user`.`user_surname`) AS patient_fullname,
+					    CONCAT(`_doctor`.`doctor_name`, ' ', `_doctor`.`doctor_surname`) AS doctor_fullname,
+					    `_appointment`.`appointment_date`,
+					    `_appointment`.`appointment_time`
+					FROM
+					    `_appointment`
+					JOIN `_doctor` ON `_doctor`.`doctor_id` = `_appointment`.`doctor_id`
+					JOIN `_user` ON `_user`.`user_id` = `_appointment`.`user_id`
+					WHERE `_appointment`.`status` = 'confirmed'
+					GROUP BY
+					    `_appointment`.`appointment_id`;
+				");
+			}
+			$stmt->execute();
+		?>
+		<?php while ($row = $stmt->fetch()): ?>
 			<hr>
 			<article class="appointment">
 				<hgroup>
-					<h2>รักชาติ บุญยิ่งใหญ่</h2>
-					<p>ต้องพบกับจิตแพทย์ เพชรกัญญา มีญาณทิพย์</p>
+					<h2><?=$row["patient_fullname"]?></h2>
+					<p>ต้องพบกับจิตแพทย์ <?=$row["doctor_fullname"]?></p>
 				</hgroup>
 				<div>
 					<div>
@@ -35,12 +88,12 @@
 							<path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
 							<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
 						</svg>
-						<time datetime="2025-08-11T13:00">11 ส.ค. 2568, 13:00 น.</time>
+						<time datetime="2025-08-11T13:00"><?=$row["appointment_date"]?>, <?=$row["appointment_time"]?></time>
 					</div>
-					<a href="#" class="button big text-center">เสร็จสิ้น</a>
+					<button onclick="confirmComplete(<?=$row['appointment_id']?>, '<?=$row['patient_fullname']?>')" class="big text-center">เสร็จสิ้น</button>
 				</div>
 			</article>
-		<?php endfor; ?>
+		<?php endwhile; ?>
 	</main>
 	<?php include "../components/footer.php"; ?>
 </body>
