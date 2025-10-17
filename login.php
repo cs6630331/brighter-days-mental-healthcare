83fb00856ddb16059a9e28c276e0a2b9baac7918
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-10-17 19:33:35 +0700

    เพิ่มปุ่มย้อนกลับสำหรับหน้าล็อกอิน

--- login.php
+++ login.php
@@ -66,6 +66,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {   //เช็คว่ากดย�
 </head>
 <body>
     <div class="container">
+        <a href="/" class="back-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+            </svg>
+        </a>
         <div class="logo">
             <h1>🌟 Brighter Days</h1>
             <p>Mental Healthcare</p>
