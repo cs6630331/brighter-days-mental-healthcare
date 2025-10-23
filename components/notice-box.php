c90be1e0acdc743e921b86fbfb49fe289a589982
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-10-23 12:00:21 +0700

    แก้ไขให้กล่องแจ้งเตือนสามารถใช้งานกับหน้าอื่นๆ ได้

--- components/notice-box.php
+++ components/notice-box.php
@@ -1,18 +1,10 @@
-<?php
-	if (!empty($_COOKIE["notice"])) {
-		$exploded_notice = explode(";", $_COOKIE["notice"]);
-		$notice = $exploded_notice[0] == "update-info" ? $exploded_notice[1] : "";
-
-		setcookie("notice", "");
-	}
-?>
-<?php if (!empty($notice)): ?>
+<?php if (!empty($_COOKIE["notice"])): ?>
 	<div class="notice">
 		<div>
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
 				<path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
 			</svg>
-			<span><?=$notice?></span>
+			<span><?=$_COOKIE["notice"]?></span>
 		</div>
 		<button class="close" onclick="this.parentElement.remove()">
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
@@ -20,4 +12,5 @@
 			</svg>
 		</button>
 	</div>
+	<?php setcookie("notice", ""); ?>
 <?php endif; ?>
