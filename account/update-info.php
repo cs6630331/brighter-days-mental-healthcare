77b960e8ffd461d3682d15b0ec9915626fcdb98b
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-10-23 11:36:21 +0700

    เพิ่มการแก้ไขข้อมูลส่วนตัวของผู้ใช้ พร้อมการแจ้งเตือน

---
+++ account/update-info.php
@@ -0,0 +1,36 @@
+<?php
+
+include "../connect.php";
+session_start();
+
+if (!isset($_SESSION["user_id"])) {
+	header("Location: /login.php");
+	die();
+}
+
+if (empty($_POST)) {
+	header("Location: my-info.php");
+	die();
+}
+
+$stmt = $pdo->prepare("UPDATE `_user` SET user_name = ?, user_surname = ?, user_tel = ?, citizen_id = ? WHERE user_id = ?");
+$stmt->bindParam(1, $_POST["name"]);
+$stmt->bindParam(2, $_POST["surname"]);
+$stmt->bindParam(3, $_POST["phone-number"]);
+$stmt->bindParam(4, $_POST["thai-id"]);
+$stmt->bindParam(5, $_SESSION["user_id"]);
+
+if ($stmt->execute()) {
+	$_SESSION['user_detail'] = [
+		"name" => $_POST["name"],
+		"surname" => $_POST["surname"],
+		"phone_number" => $_POST["phone-number"],
+		"citizen_id" => $_POST["thai-id"]
+	];
+
+	header("Location: my-info.php");
+	setcookie("notice", "update-info;อัพเดตข้อมูลสำเร็จ");
+}
+else {
+	echo "เกิดข้อผิดพลาดระหว่างอัพเดตข้อมูล";
+}
