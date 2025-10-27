1af21a5d9596d54a37aab63724e879d591b546d0
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-10-27 07:49:01 +0700

    เพิ่มการแก้ไขวันเวลานัด

---
+++ account/update-appointment.php
@@ -0,0 +1,33 @@
+<?php
+include "../connect.php";
+session_start();
+
+$stmt = $pdo->prepare("SELECT * FROM `_appointment` WHERE doctor_id = ? AND appointment_date = ? AND appointment_time = ?");
+$stmt->bindParam(1, $_SESSION["user_appointment"]["doctor_id"]);
+$stmt->bindParam(2, $_POST["date"]);
+$stmt->bindParam(3, $_POST["time"]);
+
+$stmt->execute();
+$row = $stmt->fetch();
+
+if ($row) {
+	setcookie("notice", "หมอไม่ว่างในวันเวลาที่คุณเลือก (" . $_POST["date"] . ", " . $_POST["time"] . ") กรุณาเลือกวันเวลาอื่น");
+	header("Location: /account/pospone-appointment.php");
+	die();
+}
+
+$stmt2 = $pdo->prepare("UPDATE `_appointment` SET appointment_date = ?, appointment_time = ? WHERE appointment_id = ?");
+$stmt2->bindParam(1, $_POST["date"]);
+$stmt2->bindParam(2, $_POST["time"]);
+$stmt2->bindParam(3, $_SESSION["user_appointment"]["id"]);
+
+if ($stmt2->execute()) {
+	$_SESSION["user_appointment"]["date"] = $_POST["date"];
+	$_SESSION["user_appointment"]["time"] = $_POST["time"];
+	setcookie("notice", "เลื่อนเวลานัดสำเร็จ");
+}
+else {
+	setcookie("notice", "เลื่อนเวลานัดไม่สำเร็จ ขออภัยในความไม่สะดวก");
+}
+
+header("Location: /account/my-appointment.php");
