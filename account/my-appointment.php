3bb569141961d0c800e2eb19dc510237456f19fa
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-10-27 09:01:44 +0700

    เพิ่มการยกเลิกนัด

--- account/my-appointment.php
+++ account/my-appointment.php
@@ -13,6 +13,12 @@
 	<?php include "../components/head.php"; ?>
 	<title>My Appointment | Brighter Days Mental Healthcare</title>
 	<link rel="stylesheet" href="/style/account.css">
+	<script>
+		function confirmCancel() {
+			if (confirm("คุณแน่ใจหรือไม่ว่าจะยกเลิกการนัด?"))
+				location.href = "/account/delete-appointment.php";
+		}
+	</script>
 </head>
 <body>
 	<?php include "../components/header.php"; ?>
@@ -47,7 +53,7 @@
 					<p><?=$_SESSION["user_appointment"]["notes"]?></p>
 					<div style="text-align: right;">
 						<a class="button" href="pospone-appointment.php">เลื่อนนัดหมาย</a>
-						<a class="button" href="cancel-appointment.php">ยกเลิกนัดหมาย</a>
+						<a class="button" href="javascript:confirmCancel()">ยกเลิกนัดหมาย</a>
 					</div>
 				<?php else: ?>
 					<h2 class="text-center">ไม่พบนัดหมาย</h2>
