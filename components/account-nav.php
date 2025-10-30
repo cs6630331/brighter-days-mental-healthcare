5b92c18fa67adb5b800842dcd5280aa1d36181f3
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-10-30 17:41:48 +0700

    เพิ่มลิงก์ admin ให้เข้าถึงง่าย

--- components/account-nav.php
+++ components/account-nav.php
@@ -4,6 +4,10 @@
 			<li><a href="/account/my-info.php">ข้อมูลของฉัน</a></li>
 			<li><a href="/account/my-appointment.php">นัดหมายของฉัน</a></li>
 			<li><a href="/account/change-password.php">เปลี่ยนรหัสผ่าน</a></li>
+            <?php if ($_SESSION["is_admin"]): ?>
+                <li><a href="http://202.44.40.193/~cs6630331/brighter-days-mental-healthcare/admin/appointments.php">ดูนัดหมายผู้ป่วย</a></li>
+                <li><a href="http://202.44.40.193/~cs6630331/brighter-days-mental-healthcare/admin/stats.php">ดูสถิติ</a></li>
+            <?php endif; ?>
 			<li><a href="logout.php">ออกจากระบบ</a></li>
 		</ul>
 	</nav>
