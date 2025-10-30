d7b7df80ec8fe04d21c2f3622a91bac2497207e1
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-10-30 18:03:30 +0700

    แก้กลับไปเป็น /

--- components/account-nav.php
+++ components/account-nav.php
@@ -5,8 +5,8 @@
 			<li><a href="/account/my-appointment.php">นัดหมายของฉัน</a></li>
 			<li><a href="/account/change-password.php">เปลี่ยนรหัสผ่าน</a></li>
             <?php if ($_SESSION["is_admin"]): ?>
-                <li><a href="http://202.44.40.193/~cs6630331/brighter-days-mental-healthcare/admin/appointments.php">ดูนัดหมายผู้ป่วย</a></li>
-                <li><a href="http://202.44.40.193/~cs6630331/brighter-days-mental-healthcare/admin/stats.php">ดูสถิติ</a></li>
+                <li><a href="/admin/appointments.php">ดูนัดหมายผู้ป่วย</a></li>
+                <li><a href="/admin/stats.php">ดูสถิติ</a></li>
             <?php endif; ?>
 			<li><a href="logout.php">ออกจากระบบ</a></li>
 		</ul>
